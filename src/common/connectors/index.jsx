17efaf7338d76d714230869c8982188940c71332
Isha Goyal <ishagoyal@Ishas-MacBook-Air.local>
2023-07-31 14:10:24 +0530

    Added support for different domains for different chain ids

--- src/common/connectors/index.jsx
+++ src/common/connectors/index.jsx
@@ -1,9 +1,52 @@
-import { ChainId } from '@jediswap/sdk';
-import { ArgentXConnector } from '@web3-starknet-react/argentx-connector';
-import { BraavosConnector } from '@web3-starknet-react/braavos-connector';
+import { ChainId } from "@jediswap/sdk";
+import { ArgentXConnector } from "@web3-starknet-react/argentx-connector";
+import { BraavosConnector } from "@web3-starknet-react/braavos-connector";
+
+console.log("hostname", window.location.hostname);
+export let argentX = new ArgentXConnector({
+  supportedChainIds: [ChainId.MAINNET],
+});
+export let braavosWallet = new BraavosConnector({
+  supportedChainIds: [ChainId.MAINNET],
+});
+if (window.location.hostname == "missions.jediswap.xyz") {
+  argentX = new ArgentXConnector({
+    supportedChainIds: [ChainId.MAINNET],
+  });
+  braavosWallet = new BraavosConnector({
+    supportedChainIds: [ChainId.MAINNET],
+  });
+}
+
+if (window.location.hostname == "localhost") {
+  {
+    argentX = new ArgentXConnector({
+      supportedChainIds: [ChainId.GÖRLI],
+    });
+    braavosWallet = new BraavosConnector({
+      supportedChainIds: [ChainId.GÖRLI],
+    });
+  }
+}
+
+if (window.location.hostname == "missions.testnet.jediswap.xyz") {
+  {
+    argentX = new ArgentXConnector({
+      supportedChainIds: [ChainId.GÖRLI],
+    });
+    braavosWallet = new BraavosConnector({
+      supportedChainIds: [ChainId.GÖRLI],
+    });
+  }
+}
+
+if (window.location.hostname == "mission.staging.jediswap.xyz") {
+  argentX = new ArgentXConnector({
+    supportedChainIds: [ChainId.MAINNET, ChainId.GÖRLI],
+  });
+  braavosWallet = new BraavosConnector({
+    supportedChainIds: [ChainId.MAINNET, ChainId.GÖRLI],
+  });
+}
 
 export const NETWORK_CHAIN_ID = 1;
-
-export const argentX = new ArgentXConnector({ supportedChainIds: [ChainId.MAINNET] });
-
-export const braavosWallet = new BraavosConnector({ supportedChainIds: [ChainId.MAINNET] });
