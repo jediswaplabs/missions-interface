32985c34d37a6c914c26fc0b94e474f6ac45b706
ishagoyale <iamishagoyal@gmail.com>
2023-08-26 19:04:45 +0530

    Fix contract address for testnet

--- src/pages/ProfilePage/profileAPI.js
+++ src/pages/ProfilePage/profileAPI.js
@@ -1,3 +1,5 @@
+import { testnetContractAddress } from '../../common/contansts';
+
 const options = {
   method: 'GET',
   headers: { accept: 'application/json', 'x-api-key': 'GjhopFFeeF5GM9egesqoTNzmwHUTReQ4ZgZD3RKe' },
@@ -7,7 +9,7 @@ const api = {
 
   fetchData: async (address) => {
     try {
-      const response = await fetch(`https://api-testnet.starkscan.co/api/v0/nfts?contract_address=0x06c4f71c1c4a14bba747b4d18dfb73b486aa2ba921dd0de4f64dc415536b8ba6&owner_address=${address}`, options);
+      const response = await fetch(`https://api-testnet.starkscan.co/api/v0/nfts?contract_address=${testnetContractAddress}&owner_address=${address}`, options);
       if (!response.ok) {
         throw new Error('Failed to fetch data');
       }
