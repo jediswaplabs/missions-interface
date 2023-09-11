5098342090766b804d8b24db7eecaa502a65a042
ishagoyale <iamishagoyal@gmail.com>
2023-09-11 15:35:41 +0530

    Added loader when nfts are loading in profile page

--- src/pages/ProfilePage/ProfilePage.styles.jsx
+++ src/pages/ProfilePage/ProfilePage.styles.jsx
@@ -8,14 +8,14 @@ const RoundedRect = styled(Box)`
   align-items: center;
   justify-content: center;
   border-radius: 8px;
-  border: 1px solid rgba(160, 160, 160, 0.40);
+  border: 1px solid rgba(160, 160, 160, 0.4);
   background: rgba(255, 255, 255, 0.05);
   // flex-basis: 25%;
   margin: 10px;
 `;
 const ProfileHeading = styled.div`
-  color: #FFF;
-  font-feature-settings: 'salt' on, 'clig' off, 'liga' off;
+  color: #fff;
+  font-feature-settings: "salt" on, "clig" off, "liga" off;
   font-family: DM Sans, sans-serif;
   font-size: 34px;
   font-style: normal;
@@ -23,7 +23,7 @@ const ProfileHeading = styled.div`
   line-height: 41.5px; /* 122.059% */
 `;
 const ProfileText = styled.div`
-  color: #F2F2F2;
+  color: #f2f2f2;
   text-align: center;
   font-family: DM Sans, sans-serif;
   font-size: 16px;
@@ -31,8 +31,12 @@ const ProfileText = styled.div`
   font-weight: 400;
   line-height: 26px; /* 162.5% */
 `;
+const LoadingContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 74vh;
+`;
 export {
-  RoundedRect,
-  ProfileHeading,
-  ProfileText
+  RoundedRect, ProfileHeading, ProfileText, LoadingContainer,
 };
